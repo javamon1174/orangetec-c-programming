1b976c71ec7a6693236c188a307689b48fbf0a94
javamon <javamon1174@gmail.com>
2018-12-24 06:20:51 +0900

    first commit

---
+++ cmake-build-debug/CMakeFiles/orangetec_c_programming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orangetec_c_programming.dir/main.cpp.o"
+  "orangetec_c_programming.pdb"
+  "orangetec_c_programming"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orangetec_c_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/orangetec_c_programming.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for orangetec_c_programming.
+# This may be replaced when dependencies are built.
